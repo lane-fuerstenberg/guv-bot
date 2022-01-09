b739e67574f869ead9a8c5e4c00bc174fb48954a
lane-fuerstenberg <lane.fuerstenberg@protonmail.com>
2022-01-09 15:50:25 -0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Insert your bot's token here
+        String token = "OTI5ODQxOTM0NDM0OTYzNTA3.YdtMyA.V6Yl94YbSGl3CcyvItknR7I4-dM";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
